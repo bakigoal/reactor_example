c95c57c09ff84cfefd678606e0d9bf4c7dbf9f33
Ilmir Bakirov <ilmirbakirov@gmail.com>
2022-03-30 14:02:43 +0500

    threading

---
+++ src/test/kotlin/com/bakigoal/reactor/create/ThreadingAndSchedulers.kt
@@ -0,0 +1,69 @@
+package com.bakigoal.reactor.create
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.core.publisher.Flux
+
+import reactor.core.scheduler.Schedulers
+
+
+class ThreadingAndSchedulers {
+
+    @Test
+    fun `mono in a new thread`() {
+        val mono = Mono.just("Hi there")
+        println("Created mono in thread: ${currentThread()}")
+
+        val thread = Thread {
+            mono.map { "$it from a thread" }
+                .subscribe { println("$it: ${currentThread()}") }
+        }
+
+        thread.start()
+        thread.join()
+    }
+
+    private fun currentThread() = Thread.currentThread().name
+
+    @Test
+    fun `publishOn method test`() {
+        val scheduler = Schedulers.newParallel("parallel-scheduler", 4)
+
+        val flux = Flux
+            .range(1, 2)
+            .map { i: Int ->
+                println("${currentThread()}: calculate $i")
+                10 + i
+            }
+            .publishOn(scheduler)
+            .map { i: Int ->
+                println("${currentThread()}: change to string $i")
+                "value $i"
+            }
+
+        val thread = Thread { flux.subscribe { x: String -> println("${currentThread()}: $x") } }
+        thread.start()
+        Thread.sleep(1000L)
+    }
+
+    @Test
+    fun `subscribeOn method test`() {
+        val scheduler = Schedulers.newParallel("parallel-scheduler", 4)
+
+        val flux = Flux
+            .range(1, 2)
+            .map { i: Int ->
+                println("${currentThread()}: calculate $i")
+                10 + i
+            }
+            .subscribeOn(scheduler) // Changes the Thread from which the whole chain of operators subscribes
+            .map { i: Int ->
+                println("${currentThread()}: change to string $i")
+                "value $i"
+            }
+
+        val thread = Thread { flux.subscribe { x: String -> println("${currentThread()}: $x") } }
+        thread.start()
+        Thread.sleep(1000L)
+    }
+}
